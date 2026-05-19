a4d51ba42ba9fafec6ac5cc0f149ebe8f81277ab
LautaroCenteno <63695559+LautaroCenteno@users.noreply.github.com>
2026-05-19 20:54:40 -0300

    329B pausa fix

--- 329B.cpp
+++ 329B.cpp
@@ -27,7 +27,7 @@ typedef vector<bool> vbool;
 #define S second
 
 
-void dfs(int i, int j, vvi& g, vector<vbool>& visited, vector<pi>& camino, bool c){
+void dfs(int i, int j, vector<string>& g, vector<vbool>& visited, vector<pi>& camino){
     if(visited[s]) return; // aca return false si estoy buscando ciclos
     vistied[s] = true;
     if(c){
@@ -39,67 +39,18 @@ void dfs(int i, int j, vvi& g, vector<vbool>& visited, vector<pi>& camino, bool 
     }
 }
 
-int cant_true(vbool v){
-    int res = 0;
-    for(bool b:v){
-        if(b){
-            res++;
-        }
-    }
-    return res;
-}
-
 int main() {
-    int t;
-    cin >> t;
-    int temp = t;
-    while(temp--) {
-        int n, m;
-        cin >> n >> m;
-        int mtemp = m;
-        vvi g(n);
-        //vpi rank(n);
-        vbool visited1(n);
-        vbool painted1(n);
-        vbool visited2(n);
-        vbool painted2(n);
-        /*
-        F0(i,n){
-            rank[i] = {0,i+1};
-        }
-        */
-        while(mtemp--){
-            int x, y;
-            cin >> x >> y;
-            g[x-1].pb(y);
-            g[y-1].pb(x);
-            //rank[x-1].first++;
-            //rank[y-1].first++;
-        }
-
-        /*
-        sort(rank.begin(),rank.end());
-        reverse(rank.begin(),rank.end());
-        */
-
-        /*
-        F0(i,n){
-            cout << rank[i].first << " " << rank[i].second << "\n";
-        }
-        */
-
-        dfs_paint(0,g,visited1,painted1,true);
-        dfs_paint(0,g,visited1,painted1,false);
-
-        if(cant_true(painted1) < cant_true(painted2)){
-            cout << cant_true(painted1) << "\n";
-            for(i = 0; i < painted1.size(); i++){
-                if(painted1[i]){
-                    cout << (i+1) << " ";
-                } 
-            }
-        }
-        
+    int r;
+    int c;
+    cin >> r >> c;
+    int temp = r;
+    vector<string> g;
+    F0(i,r){
+        string s;
+        cin >> s;
+        g[i] = s;
     }
+    //caminos(...)
+    
     return 0;
 }
