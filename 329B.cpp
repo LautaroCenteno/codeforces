6799eb765fa6ecce34432ae03dffd4a65dd5583c
LautaroCenteno <lautarocenteno.lc@gmail.com>
2026-05-20 23:57:00 -0300

    329B fin

--- 329B.cpp
+++ 329B.cpp
@@ -28,30 +28,40 @@ typedef vector<bool> vbool;
 
 void bfs_dist(pi s, vector<string>& g, vvi& g_dist, vector<vbool>& visited){
     queue<pi> q;
-    int pasos = 0;
     visited[s.first][s.second] = true;
-    g_dist[s.first][s.second] = pasos;
+    g_dist[s.first][s.second] = 0;
     q.push(s);
     while(!q.empty()){
-        pasos++;
-        vpi vecinos;
-        if((q.front().first + 1) < g.size() && g[q.front().first + 1][q.front().second] != 'T') vecinos.pb({q.front().first + 1,q.front().second});
-        if((q.front().first - 1) >= 0 && g[q.front().first - 1][q.front().second] != 'T') vecinos.pb({q.front().first - 1,q.front().second});
-        if((q.front().second + 1) < g[q.front().first].size() && g[q.front().first][q.front().second + 1] != 'T') vecinos.pb({q.front().first,q.front().second + q.front().second});
-        if((q.front().second - 1) >= 0 && g[q.front().first][q.front().second - 1] != 'T') vecinos.pb({q.front().first,q.front().second - 1});
-        q.pop();
-        for(int i = 0; i < vecinos.size(); i++){
-            if(visited[vecinos[i].first][vecinos[i].second]) continue;
-            g_dist[vecinos[i].first][vecinos[i].second] = pasos;
-            visited[vecinos[i].first][vecinos[i].second] = true;
-            q.push(vecinos[i]);
+        if((q.front().first + 1) < g.size() && g[q.front().first + 1][q.front().second] != 'T' && !visited[q.front().first + 1][q.front().second]) {
+            g_dist[q.front().first + 1][q.front().second] = g_dist[q.front().first][q.front().second] + 1;
+            visited[q.front().first + 1][q.front().second] = true;
+            q.push({q.front().first + 1, q.front().second});
         }
+        if((q.front().first - 1) >= 0 && g[q.front().first - 1][q.front().second] != 'T' && !visited[q.front().first - 1][q.front().second]){
+            g_dist[q.front().first - 1][q.front().second] = g_dist[q.front().first][q.front().second] + 1;
+            visited[q.front().first - 1][q.front().second] = true;
+            q.push({q.front().first - 1, q.front().second});
+        } 
+        if((q.front().second + 1) < g[q.front().first].size() && g[q.front().first][q.front().second + 1] != 'T' && !visited[q.front().first][q.front().second + 1]){
+            g_dist[q.front().first][q.front().second + 1] = g_dist[q.front().first][q.front().second] + 1;
+            visited[q.front().first][q.front().second + 1] = true;
+            q.push({q.front().first, q.front().second + 1});
+        } 
+        if((q.front().second - 1) >= 0 && g[q.front().first][q.front().second - 1] != 'T' && !visited[q.front().first][q.front().second - 1]){
+            g_dist[q.front().first][q.front().second - 1] = g_dist[q.front().first][q.front().second] + 1;
+            visited[q.front().first][q.front().second - 1] = true;
+            q.push({q.front().first, q.front().second - 1});
+        }
+        q.pop();
     }
 }
 
 void dfs_dist(int i, int j, int pasos, vector<string>& g, vvi& g_dist, vector<vbool>& visited){
     if(visited[i][j]) return; // aca return false si estoy buscando ciclos
-    if(g[i][j] != 'T') g_dist[i][j] = pasos;
+    if(g[i][j] != 'T'){
+        g_dist[i][j] = pasos;
+        return;
+    }
     visited[i][j] = true;
     pasos++;
     if((i+1) < g.size() && g[i+1][j] != 'T') dfs_dist((i+1), (j), pasos, g, g_dist, visited);
@@ -60,18 +70,17 @@ void dfs_dist(int i, int j, int pasos, vector<string>& g, vvi& g_dist, vector<vb
     if((j-1) >= 0 && g[i][j-1] != 'T') dfs_dist((i), (j-1), pasos, g, g_dist, visited);
 }
 
-void dfs_caminos(int i, int j, int pasos, int& menor_pasos, vector<string>& g, vector<vbool>& visited){
+void dfs_caminos(int i, int j, int pasos, int& menor_pasos, vector<string>& g, vector<vbool>& visited, vvi& g_dist){
     if(visited[i][j]) return; // aca return false si estoy buscando ciclos
     if(g[i][j] == 'E'){
         if(pasos < menor_pasos) menor_pasos = pasos;
         return;
     }
-    pasos++;
     visited[i][j] = true;
-    if((i+1) < g.size() && g[i+1][j] != 'T') dfs_caminos((i+1), (j), pasos, menor_pasos, g, visited);
-    if((i-1) >= 0 && g[i-1][j] != 'T') dfs_caminos((i-1), (j), pasos, menor_pasos, g, visited);
-    if((j+1) < g[i].size() && g[i][j+1] != 'T') dfs_caminos((i), (j+1), pasos, menor_pasos, g, visited);
-    if((j-1) >= 0 && g[i][j-1] != 'T') dfs_caminos((i), (j-1), pasos, menor_pasos, g, visited);
+    if((i+1) < g.size() && g[i+1][j] != 'T' && g_dist[i+1][j] < g_dist[i][j]) dfs_caminos((i+1), (j), (pasos+1), menor_pasos, g, visited, g_dist);
+    if((i-1) >= 0 && g[i-1][j] != 'T' && g_dist[i-1][j] < g_dist[i][j]) dfs_caminos((i-1), (j), (pasos+1), menor_pasos, g, visited, g_dist);
+    if((j+1) < g[i].size() && g[i][j+1] != 'T' && g_dist[i][j+1] < g_dist[i][j]) dfs_caminos((i), (j+1), (pasos+1), menor_pasos, g, visited, g_dist);
+    if((j-1) >= 0 && g[i][j-1] != 'T' && g_dist[i][j-1] < g_dist[i][j]) dfs_caminos((i), (j-1), (pasos+1), menor_pasos, g, visited, g_dist);
 }
 
 pi pos_start(vector<string>& g){
@@ -134,7 +143,7 @@ int main() {
     vector<pi> breeders;
     int menor_peleas = 0;
     int menor_pasos = r*c + 1;
-    vvi g_dist(r, vi(c));
+    vvi g_dist(r, vi(c, (-1)));
 
     F0(i,g.size()){
         F0(j,g[i].size()){
@@ -142,15 +151,14 @@ int main() {
         }
     }
 
-    dfs_caminos(pos_s.first,pos_s.second,0,menor_pasos,g,visited);
-
     vector<vbool> visited2(r, vbool(c));
     bfs_dist(pos_e,g,g_dist,visited2);
-
+   
+    dfs_caminos(pos_s.first,pos_s.second,0,menor_pasos,g,visited,g_dist);
     
     F0(i,g.size()){
         F0(j,g[i].size()){
-            if(g[i][j] != 'T' && g[i][j] != 'S' && g[i][j] != 'E' && g[i][j] != '0' && g_dist[i][j] <= menor_pasos){
+            if(g[i][j] != 'T' && g[i][j] != 'S' && g[i][j] != 'E' && g[i][j] != '0' && g_dist[i][j] != (-1) && g_dist[i][j] <= menor_pasos){
                 menor_peleas += g[i][j] - '0';
             }
         }
@@ -210,14 +218,16 @@ int main() {
     }
     */
 
+    /* FUNCIONA
     F0(i,g_dist.size()){
         F0(j,g_dist[i].size()){
             cout << g_dist[i][j] << " ";
         }
         cout << "\n";
     }
-
-    //cout << menor_peleas;
+    */
+    
+    cout << menor_peleas;
     
     return 0;
 }
