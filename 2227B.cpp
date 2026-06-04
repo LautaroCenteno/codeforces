00323b3ffd79a586ee2ddb8b006962d3669f7183
LautaroCenteno <lautarocenteno.lc@gmail.com>
2026-06-04 15:43:51 -0300

    2227 ejercicios

---
+++ 2227B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pd;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef vector<bool> vbool;
+ 
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define forall(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define F0(i,n) for(int i = 0; i < n; i++)
+#define F1(i,n) for(int i = 1; i <= n; i++)
+#define REP(i,a,b) for(int i = a; i <= b; i++)
+#define F first
+#define S second
+
+int main() {
+    int t;
+    cin >> t;
+    F0(i,t){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int l = 0;
+        int r = 0;
+        F0(j,n){
+            if(s[j] == '(') l += 1;
+            else{
+                r += 1;
+            }
+        }
+        if(l != r) cout << "no" << "\n";
+        else cout << "yes" << "\n";
+    }
+}
